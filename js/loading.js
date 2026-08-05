207e02229d8b993ef5b47e6a7389e9a3665da182
inuwanwan-roco <gb.laaav.can@gmail.com>
2026-08-06 00:49:38 +0900

    git commit -m "ローディング画面実装と注文サマリー共通化"

---
+++ js/loading.js
@@ -0,0 +1,35 @@
+// ==========================================
+// Loading Animation
+// ==========================================
+
+const footprints = document.querySelector(
+    ".p-loading__footprints"
+);
+
+const maxFootprints = 6;
+
+let count = 0;
+
+const interval = setInterval(() => {
+
+    const paw = document.createElement("i");
+
+    paw.className = "fa-solid fa-paw p-loading__paw";
+
+    footprints.appendChild(paw);
+
+    count++;
+
+    if (count >= maxFootprints) {
+
+        clearInterval(interval);
+
+        setTimeout(() => {
+
+            window.location.href = "complete.html";
+
+        }, 1000);
+
+    }
+
+}, 350);
